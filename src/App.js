c863104ce069f07203b289b062ce233d9e4afc0a
Jonathan <grigsbyjonathan@gmail.com>
2022-02-16 08:02:50 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, Navbar, Cart, Checkout } from "./components";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
+
+    const fetchProducts = async () => {
+        //await returned products
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    };
+    // console.log(data);
+
+    const fetchCart = async () => {
+        //putting res in var is redundant, so we can set cart to the state
+        setCart(await commerce.cart.retrieve());
+    };
+
+    const handleAddToCart = async (productId, quantity) => {
+        const { cart } = await commerce.cart.add(productId, quantity);
+        setCart(cart);
+    };
+
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, { quantity });
+        setCart(cart);
+    };
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
+        setCart(cart);
+    };
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty();
+        setCart(cart);
+    };
+
+    useEffect(() => {
+        //setting states
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+    // console.log(cart.total_items);
+
+    return (
+        <Router>
+            <div>
+                <Navbar totalItems={cart.total_items} />
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            <Products
+                                products={products}
+                                onAddToCart={handleAddToCart}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/cart"
+                        element={
+                            <Cart
+                                cart={cart}
+                                onUpdateCartQty={handleUpdateCartQty}
+                                onEmptyCart={handleEmptyCart}
+                                onRemoveFromCart={handleRemoveFromCart}
+                            />
+                        }
+                    />
+                    <Route path="/checkout" element={<Checkout />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+    AppBar,
+    Toolbar,
+    IconButton,
+    Badge,
+    MenuItem,
+    Menu,
+    Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { Link, useLocation } from "react-router-dom";
+
+import useStyles from "./styles";
+
+const Navbar = ({ totalItems }) => {
+    const classes = useStyles();
+    const location = useLocation();
+
+    return (
+        <>
+            <AppBar position="fixed" className={classes.AppBar} color="inherit">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        className={classes.title}
+                        color="inherit"
+                        component={Link}
+                        to="/"
+                    >
+                        <img
+                            src=""
+                            alt=""
+                            height="25px"
+                            className={classes.image}
+                        />
+                        BUYSTUFF
+                    </Typography>
+                    <div className={classes.grow} />
+
+                    {location.pathname === "/" && (
+                        <div className={classes.button}>
+                            <IconButton
+                                component={Link}
+                                to="/cart"
+                                color="inherit"
+                            >
+                                <Badge
+                                    badgeContent={totalItems}
+                                    color="secondary"
+                                >
+                                    <ShoppingCart />
+                                </Badge>
+                            </IconButton>
+                        </div>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+};
+
+export default Navbar;
