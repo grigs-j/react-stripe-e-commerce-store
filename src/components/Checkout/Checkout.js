1fc8d6300dbf21a6ebcdef2cc229692d29476388
Jonathan <grigsbyjonathan@gmail.com>
2022-02-16 15:27:03 -0800

    adding checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     Paper,
     Stepper,
@@ -9,12 +9,58 @@ import {
     Button,
     Typography,
 } from "@material-ui/core";
+import AddressForm from "../CheckoutForm/AddressForm";
+import PaymentForm from "../CheckoutForm/PaymentForm";
+import { commerce } from "../../lib/commerce";
 import useStyles from "./styles";
 
 const steps = ["Shipping address", "Payment details"];
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
     const classes = useStyles();
+
+    const [activeStep, setActiveStep] = useState(0);
+    const [checkoutToken, setCheckoutToken] = useState(null);
+    const [shippingData, setShippingData] = useState({});
+
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, {
+                    type: "cart",
+                });
+
+                // console.log(token);
+                setCheckoutToken(token);
+            } catch (error) {}
+        };
+
+        generateToken();
+    }, [cart]);
+
+    const nextStep = () =>
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    const backStep = () =>
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+    const next = (data) => {
+        setShippingData(data);
+        nextStep();
+    };
+
+    const Confirmation = () => <div>Confirmation</div>;
+
+    const Form = () =>
+        activeStep === 0 ? (
+            <AddressForm checkoutToken={checkoutToken} next={next} />
+        ) : (
+            <PaymentForm
+                shippingData={shippingData}
+                checkoutToken={checkoutToken}
+                backStep={backStep}
+            />
+        );
+
     return (
         <>
             <div className={classes.toolbar} />
@@ -30,6 +76,11 @@ const Checkout = () => {
                             </Step>
                         ))}
                     </Stepper>
+                    {activeStep === steps.length ? (
+                        <Confirmation />
+                    ) : (
+                        checkoutToken && <Form />
+                    )}
                 </Paper>
             </main>
         </>
